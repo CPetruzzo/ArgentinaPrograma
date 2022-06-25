b6979595f35e7bc68a2cd8c35f36bf756f2babb5
CPetruzzo <facu03@gmail.com>
2022-06-25 15:18:41 -0300

    Components

--- proyectofinal/src/app/app.module.ts
+++ proyectofinal/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HardAndSkillsComponent } from './components/hard-and-skills/hard-and-skills.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AboutMeComponent,
+    FooterComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardAndSkillsComponent,
+    ProyectsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
